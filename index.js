4d5b1cb3a0550a8fb7f0c11eed957b5d9acddc68
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2022-12-27 18:44:04 +0200

    Step 2

---
+++ index.js
@@ -0,0 +1,30 @@
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+// const http = require("http");
+
+// const server = http.createServer();
+
+// server.listen(3000);
+
+// app.get("/ahmad", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// server.listen(3000);
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Ok");
+});
+
+app.get("/greeting", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
