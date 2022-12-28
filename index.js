2b6cbf3b06560d8b2885d5835597fc0e4ff10975
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2022-12-28 14:13:26 +0200

    Step 5

--- index.js
+++ index.js
@@ -74,6 +74,29 @@ app.get("/search", (req, res) => {
   }
 });
 
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب", year: 1992, rating: 6.2 },
+];
+
+app.get("/movies/create", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/movies/read", (req, res) => {
+  res.send({ status: 200, data: movies });
+});
+
+app.get("/movies/update", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/movies/delete", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
